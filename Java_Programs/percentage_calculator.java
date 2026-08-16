06985e28d9a2a3ea92264fde684980de7d098d73
Taha Ansari <tahairfanansari@gmail.com>
2026-08-16 18:52:12 +0530

    Create percentage_calculator.java

---
+++ Java_Programs/percentage_calculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class percentage_cal {
+    public static void main(String[] args){
+        System.out.println("PERCENTAGE CALCULATOR");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Marks 1: ");
+        float m1 = sc.nextFloat();
+        System.out.println("Enter Marks 2: ");
+        float m2 = sc.nextFloat();
+        System.out.println("Enter Marks 3: ");
+        float m3 = sc.nextFloat();
+        System.out.println("Enter Marks 4: ");
+        float m4 = sc.nextFloat();
+        System.out.println("Enter Marks 5: ");
+        float m5 = sc.nextFloat();
+
+        float marks_obt = m1+m2+m3+m4+m5;
+        float form = (marks_obt/500) * 100;
+        System.out.println("Percentage: "+ form+"%");
+        
+
+    }
+}
