8e81027d18cb26235af5cb9da20c335e4281d7a0
Taha Ansari <tahairfanansari@gmail.com>
2026-08-16 18:34:48 +0530

    Add integer validity check in validity.java
    Implement basic input validation for integers.

---
+++ validity.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class validity {
+    public static void main(String[] args){
+        System.out.println("validity");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        boolean b1 = sc.hasNextInt();
+        System.out.println(b1);
+    }
+}
