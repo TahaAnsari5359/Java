0bf26909a6aa1a121526ff22157e271a4b3fd272
Taha Ansari <tahairfanansari@gmail.com>
2026-08-31 14:54:33 +0530

    Add Conditionals example in Conditionals.java

---
+++ Conditionals.java
@@ -0,0 +1,21 @@
+public class conditionals {
+    public static void main(String []args){
+        System.out.println("Conditionals in JAVA: ");
+        System.out.println("If Else If and Else Conditional");
+        int age = 10;
+        if (age>=18){
+            System.out.println("You can Drive Vehicle");
+        }
+
+        else if(age<0){
+            System.out.println("Error Wrong Age");
+        }
+
+        else{
+            System.out.println("You Cannot Drive");
+        }
+
+        System.out.println("Switch Statements: ");
+
+    }
+}
