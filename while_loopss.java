06fbe1930a5b7365e9363c9fa5535e6b3ab5de5d
Taha Ansari <tahairfanansari@gmail.com>
2026-09-08 14:45:11 +0530

    Add while loop to print numbers from 1 to 10

---
+++ while_loopss.java
@@ -0,0 +1,9 @@
+public class while_loopss {
+    public static void main(String [] args) {
+        int i = 1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
