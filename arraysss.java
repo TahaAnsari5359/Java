cf277f1b49dadf3042347cfa687972842d54d01e
Taha Ansari <tahairfanansari@gmail.com>
2026-09-08 16:10:31 +0530

    Enhance array handling with for-each and reverse loops
    Added examples of for-each loops and reverse printing for arrays.

--- arraysss.java
+++ arraysss.java
@@ -12,6 +12,30 @@ public class arrrayss {
         int[] data = {25,30,35,40,45,50};
         System.out.println(data[5]);
 
+        //for each loops in array
+        System.out.println("For Each loops");
+        int[] test = {10,20,30,40};
+        System.out.println(test.length);
+
+        System.out.println("STring array- ");
+        String[] subsjects = {"English","Maths","hindi","CS"};
+        System.out.println(subsjects[2]);
+
+        //displaying array using for loops-
+        System.out.println("Printing using For loops - ");
+        for(int i =0; i<subsjects.length;i++){
+            System.out.println(subsjects[i]);
+        }
+
+        System.out.println("Printing in reverse Order");
+        for(int i=subsjects.length-1; i>=0;  i--){
+            System.out.println(subsjects[i]);
+        }
+
+
+
+
+
 
     }
 }
