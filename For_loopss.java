2e7f91e7a3f65466c1edc46eea3314db63cb63a5
Taha Ansari <tahairfanansari@gmail.com>
2026-09-08 14:44:39 +0530

    Add for loop to print multiplication table of 2

---
+++ For_loopss.java
@@ -0,0 +1,7 @@
+public class forr_loopsss {
+    public static void main(String []args){
+        for(int i = 0; i<=10; i++){
+            System.out.println("2 X " + i + " = " + 2*i);
+        }
+    }
+}
