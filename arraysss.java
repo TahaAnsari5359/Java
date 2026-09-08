9b1eb29c83d860dda132a52b1bd1d892300746a4
Taha Ansari <tahairfanansari@gmail.com>
2026-09-08 15:50:35 +0530

    Create arraysss.java with array examples
    Added a Java class demonstrating array declaration and data access.

---
+++ arraysss.java
@@ -0,0 +1,17 @@
+public class arrrayss {
+    public static void main(String []args){
+        //3 ways to declare
+        int[] marks;
+        marks = new int[5];
+
+        int [] marks2 = new int[5];
+
+        int[] marks3 = {1,2,3,4,5};
+
+        //adding data
+        int[] data = {25,30,35,40,45,50};
+        System.out.println(data[5]);
+
+
+    }
+}
